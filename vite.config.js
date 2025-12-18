1c89656228cccd10a2c73d85786a0868c0e94e21
Emmanuel olasunkanmi <soluwatoyin10@gmail.com>
2025-12-18 12:58:24 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import process from 'process'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: process.env.VITE_BASE_PATH || '/my-portfolio/',
   plugins: [react()],
-})
+});
