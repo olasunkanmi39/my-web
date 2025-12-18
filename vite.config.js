7dc61b67f2ba56a0daeb2df907af578f33f83194
Emmanuel olasunkanmi <soluwatoyin10@gmail.com>
2025-12-18 13:24:17 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import process from 'process'
 
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH || '/my-web/',
   plugins: [react()],
-});
+})
